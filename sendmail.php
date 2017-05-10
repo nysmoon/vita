4cca418dba040ff0af00d34b58e8d07c9d1c66b5
Natasha Sukhinina <natashas@openkey.io>
2017-05-10 18:17:30 -0500

    Added Captcha

--- sendmail.php
+++ sendmail.php
@@ -18,6 +18,26 @@ $email   = !empty($_REQUEST['email'])   ? $_REQUEST['email']   : '-';
 $message = !empty($_REQUEST['message']) ? $_REQUEST['message'] : '-';
 $form    = !empty($_REQUEST['form'])    ? $_REQUEST['form']    : '-';
 
+// Google reCAPTCHA
+
+$captcha;
+if(isset($_POST['g-recaptcha-response'])){
+  $captcha=$_POST['g-recaptcha-response'];
+}
+if(!$captcha){
+  echo '<h2>Please check the the captcha form.</h2>';
+  exit;
+}
+$secretKey = "6Lf94CAUAAAAADoKfh5pSWGJiVEDcpLNrHqd24wM";
+$ip = $_SERVER['REMOTE_ADDR'];
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+$responseKeys = json_decode($response,true);
+if(intval($responseKeys["success"]) !== 1) {
+    echo '<h2>You are spammer!</h2>';
+} else {
+  echo '<h2>Thanks for posting comment.</h2>';
+}
+
 // Forming Message
 $text = "
 Hello Vita!
@@ -39,4 +59,4 @@ ob_end_clean();
 echo json_encode(array(
     'message_send' => $result ? 'ok' : 'error',
     'hash'         => substr(md5(time()), 3, 7)
-));
+    ));
